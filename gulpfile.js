deffdf5ae6b785f79e55cc75098e3d8f5df81633
Kotmail <ya.kotmail@yandex.ru>
2023-08-10 14:26:44 +0300

    Added a server (Browsersync).

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 import gulp from 'gulp';
 import pug from 'gulp-pug';
+import browserSync from 'browser-sync';
+
+// BrowserSync.
+export const browserSyncServe = () => {
+  return browserSync.init({
+    server: './public',
+    watch: true,
+  });
+};
 
 // Pug.
 export const compilePug = () => {
@@ -9,3 +18,19 @@ export const compilePug = () => {
     }))
     .pipe(gulp.dest('./public/'))
 }
+
+// Watch.
+export const watch = () => {
+  gulp.watch('./src/pug/**/*.pug', gulp.series(compilePug));
+}
+
+// Default.
+export default gulp.series(
+  gulp.parallel(
+    compilePug
+  ),
+  gulp.parallel(
+    watch,
+    browserSyncServe
+  )
+);
