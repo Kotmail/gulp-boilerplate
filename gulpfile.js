2b441d26a133a3575a0ba49ca2f1ddcacd70fd5e
Kotmail <ya.kotmail@yandex.ru>
2023-08-11 14:44:00 +0300

    Added a task for compiling Scss.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 import gulp from 'gulp';
 import pug from 'gulp-pug';
 import browserSync from 'browser-sync';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import postCss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import styleLint from 'gulp-stylelint-esm'
+
+const sass = gulpSass(dartSass);
 
 // BrowserSync.
 export const browserSyncServe = () => {
@@ -16,19 +24,45 @@ export const compilePug = () => {
     .pipe(pug({
       pretty: true,
     }))
-    .pipe(gulp.dest('./public/'))
+    .pipe(gulp.dest('./public/'));
+}
+
+// Scss
+export const compileScss = () => {
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postCss([
+      autoprefixer(),
+    ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./public/'));
+}
+
+// Scss linting.
+export const lintScss = () => {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(styleLint({
+      reporters: [
+        {
+          formatter: 'string',
+          console: true,
+        }
+      ]
+    }));
 }
 
 // Watch.
 export const watch = () => {
-  gulp.watch('./src/pug/**/*.pug', gulp.series(compilePug));
+  gulp.watch('./src/pug/**/*.pug', compilePug);
+  gulp.watch('./src/scss/**/*.scss', gulp.series(lintScss, compileScss));
 }
 
 // Default.
 export default gulp.series(
-  gulp.parallel(
-    compilePug
-  ),
+  compilePug,
+  lintScss,
+  compileScss,
   gulp.parallel(
     watch,
     browserSyncServe
