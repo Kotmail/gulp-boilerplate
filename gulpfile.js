9cc44587d9ef6369bd0bd6d0ec16b31a420583c5
Kotmail <ya.kotmail@yandex.ru>
2023-08-12 15:31:52 +0300

    Added tasks for working with images (compressing, svg-sprite).

--- gulpfile.js
+++ gulpfile.js
@@ -8,41 +8,43 @@ import postCss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import styleLint from 'gulp-stylelint-esm'
 import { deleteAsync } from 'del';
+import imagemin from 'gulp-imagemin';
+import svgSprite from 'gulp-svg-sprite';
 
 const sass = gulpSass(dartSass);
 
 // BrowserSync.
 export const browserSyncServe = () => {
   return browserSync.init({
-    server: './public',
+    server: 'public',
     watch: true,
   });
 };
 
 // Pug.
 export const compilePug = () => {
-  return gulp.src('./src/pug/pages/*.pug')
+  return gulp.src('src/pug/pages/*.pug')
     .pipe(pug({
       pretty: true,
     }))
-    .pipe(gulp.dest('./public/'));
+    .pipe(gulp.dest('public'));
 }
 
 // Scss
 export const compileScss = () => {
-  return gulp.src('./src/scss/*.scss')
+  return gulp.src('src/scss/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(postCss([
       autoprefixer(),
     ]))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./public/'));
+    .pipe(gulp.dest('public'));
 }
 
 // Scss linting.
 export const lintScss = () => {
-  return gulp.src('./src/scss/**/*.scss')
+  return gulp.src('src/scss/**/*.scss')
     .pipe(styleLint({
       reporters: [
         {
@@ -53,11 +55,38 @@ export const lintScss = () => {
     }));
 }
 
+// Compress images.
+export const compressImages = () => {
+  return gulp.src('src/images/*.+(jpg|jpeg|png|gif)')
+    .pipe(imagemin())
+    .pipe(gulp.dest('public/images'));
+}
+
+// Svg sprite.
+export const generateSvgSprite = () => {
+  return gulp.src('src/images/svg/sprite-icons/*.svg')
+    .pipe(svgSprite({
+      mode: {
+        symbol: {
+          dest: '.',
+          sprite: 'sprite.svg',
+        },
+      },
+    }))
+    .pipe(gulp.dest('public/images/svg'));
+}
+
 // Copying folders.
-export const copyFolders = folders => {
+export const copyFolders = (folders, excludeFolders) => {
+  const paths = [`src/+(${folders.join('|')})/**`];
+
+  if (excludeFolders && excludeFolders.length) {
+    excludeFolders.forEach(item => paths.push(`!src/${item}`));
+  }
+
   const copyFolders = () =>  {
-    return gulp.src(`./src/{${folders.join(',')}}/**`)
-      .pipe(gulp.dest('./public'));
+    return gulp.src(paths)
+      .pipe(gulp.dest('public'));
   }
 
   return copyFolders;
@@ -70,21 +99,30 @@ export const removeFolders = folders => {
   return removeFolders;
 };
 
-// Watch.
-export const watch = () => {
-  gulp.watch('./src/pug/**/*.pug', compilePug);
-  gulp.watch('./src/scss/**/*.scss', gulp.series(lintScss, compileScss));
+// Watcher.
+export const watcher = () => {
+  gulp.watch('src/pug/**/*.pug', compilePug);
+  gulp.watch('src/scss/**/*.scss', gulp.series(lintScss, compileScss));
+  gulp.watch('src/images/*.+(jpg|jpeg|png|gif)', { events: ['add', 'change'] }, compressImages);
+  gulp.watch('src/images/svg/sprite-icons/*.svg', generateSvgSprite);
 }
 
 // Default.
 export default gulp.series(
   removeFolders(['public']),
-  copyFolders(['fonts']),
-  compilePug,
   lintScss,
-  compileScss,
   gulp.parallel(
-    watch,
+    copyFolders(
+      ['fonts', 'images'],
+      ['images/svg/sprite-icons/**']
+    ),
+    compilePug,
+    compileScss,
+    compressImages,
+    generateSvgSprite,
+  ),
+  gulp.parallel(
+    watcher,
     browserSyncServe
   )
 );
